4e378402d3da6d237c44cce87f61f068f4686091
SanjayTUnagar <sanjayunagar025@gmail.com>
2026-03-14 01:27:02 +0530

    initial update

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,53 +1,47 @@
-
 class Portfolio {
-
-    constructor(tabs) {
-        this.bindEvents();
-        this.tabs = tabs;
-        for(const tab of tabs){
-            this.loadData(tab);
-        }
-
-        setTimeout(()=>{
-            $('#loader').addClass("hide");
-            $('#posts').removeClass("hide");
-        }, 1000);
-
-        return this;
-
+  constructor(tabs) {
+    this.bindEvents();
+    this.tabs = tabs;
+    for (const tab of tabs) {
+      this.loadData(tab);
     }
 
-    bindEvents(){
+    setTimeout(() => {
+      $("#loader").addClass("hide");
+      $("#posts").removeClass("hide");
+    }, 1000);
 
-        $('.connect, .cross').on('click',()=>{
-            $('#connectSection').toggleClass("hide");
-            $('#icon-connect').toggleClass("fa-minus-circle");
-            $('#icon-connect').toggleClass("fa-plus-circle");
-            $('.connect').toggleClass("active");
-            
-        });
+    return this;
+  }
 
+  bindEvents() {
+    $(".connect, .cross").on("click", () => {
+      $("#connectSection").toggleClass("hide");
+      $("#icon-connect").toggleClass("fa-minus-circle");
+      $("#icon-connect").toggleClass("fa-plus-circle");
+      $(".connect").toggleClass("active");
+    });
+  }
+
+  loadData(tab) {
+    fetch(`./data/${tab}.json`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.showData(data, tab);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+
+  showData(posts, tab) {
+    for (const post of posts) {
+      $(`#${tab}`).append(this.getPost(post));
     }
+  }
 
-    loadData(tab){
-        fetch(`./data/${tab}.json`)
-        .then((resp) => resp.json())
-        .then((data)=> {
-            this.showData(data, tab);
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
-    }
-
-    showData(posts, tab){
-        for(const post of posts){
-            $(`#${tab}`).append(this.getPost(post));
-        }
-    }
-
-    getPost(post){
-        let html = `
+  getPost(post) {
+    let html = `
             <div class="post-container">
                 <div class="post-header">
                     <div class="post-header-top heading">
@@ -80,60 +74,57 @@ class Portfolio {
             </div>
         `;
 
-        return html;
+    return html;
+  }
+
+  openTab(event, tabName) {
+    $("#loader").removeClass("hide");
+    $("#posts").addClass("hide");
+
+    setTimeout(() => {
+      $("#loader").addClass("hide");
+      $("#posts").removeClass("hide");
+    }, 500);
+
+    let tabcontent, tablinks;
+
+    $(".tabcontent").removeClass("show");
+    $(`#${tabName}`).addClass("show");
+
+    tablinks = document.getElementsByClassName("menu-item");
+    for (let i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    event.currentTarget.className += " active";
+  }
+
+  sticky_relocate() {
+    let window_top = $(window).scrollTop();
+    let div_top = $("#sticky-anchor").offset().top;
+    let div_top_right = $("#sticky-anchor-right").offset().top;
+    let div_menu = $("#sticky-anchor-menu").offset().top;
+    if (window_top > div_top) {
+      $("#sticky").addClass("stick");
+      $("#sticky-anchor").height($("#sticky").outerHeight());
+    } else {
+      $("#sticky").removeClass("stick");
+      $("#sticky-anchor").height(0);
     }
 
-
-    openTab(event, tabName) {
-
-        $('#loader').removeClass("hide");
-        $('#posts').addClass("hide");
-
-        setTimeout(()=>{
-            $('#loader').addClass("hide");
-            $('#posts').removeClass("hide");
-        }, 500);
-
-        let tabcontent, tablinks;
-
-        $('.tabcontent').removeClass("show");
-        $(`#${tabName}`).addClass("show");
-
-        tablinks = document.getElementsByClassName("menu-item");
-        for (let i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-        event.currentTarget.className += " active";
+    if (window_top > div_top_right) {
+      $("#sticky-right").addClass("stick");
+      $("#sticky-anchor-right").height($("#sticky-right").outerHeight());
+    } else {
+      $("#sticky-right").removeClass("stick");
+      $("#sticky-anchor-right").height(0);
     }
 
-    sticky_relocate() {
-        let window_top = $(window).scrollTop();
-        let div_top = $('#sticky-anchor').offset().top;
-        let div_top_right = $('#sticky-anchor-right').offset().top;
-        let div_menu = $('#sticky-anchor-menu').offset().top;
-        if (window_top > div_top) {
-            $('#sticky').addClass('stick');
-            $('#sticky-anchor').height($('#sticky').outerHeight());
-        } else {
-            $('#sticky').removeClass('stick');
-            $('#sticky-anchor').height(0);
-        }
-        
-        if (window_top > div_top_right) {
-            $('#sticky-right').addClass('stick');
-            $('#sticky-anchor-right').height($('#sticky-right').outerHeight());
-        } else {
-            $('#sticky-right').removeClass('stick');
-            $('#sticky-anchor-right').height(0);
-        }
-    
-        if (window_top > div_menu) {
-            $('#sticky-menu').addClass('stick-menu');
-            $('#sticky-anchor-menu').height($('#sticky-menu').outerHeight());
-        } else {
-            $('#sticky-menu').removeClass('stick-menu');
-            $('#sticky-anchor-menu').height(0);
-        }
+    if (window_top > div_menu) {
+      $("#sticky-menu").addClass("stick-menu");
+      $("#sticky-anchor-menu").height($("#sticky-menu").outerHeight());
+    } else {
+      $("#sticky-menu").removeClass("stick-menu");
+      $("#sticky-anchor-menu").height(0);
     }
-
+  }
 }
